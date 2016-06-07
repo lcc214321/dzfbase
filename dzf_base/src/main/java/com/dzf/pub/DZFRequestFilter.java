4c04ed8b3da300b40182e2a5a289ad9e914264f8
zpm <zpm@ynt.com>
2016-06-07 18:10:48 +0800

    zpm修改提交
    Signed-off-by: zpm <zpm@ynt.com>

--- dzf_base/src/main/java/com/dzf/pub/DZFRequestFilter.java
+++ dzf_base/src/main/java/com/dzf/pub/DZFRequestFilter.java
@@ -90,6 +90,12 @@ public class DZFRequestFilter implements Filter {
 				filterChain.doFilter(request, response);
 	        	return;
 	        }
+			//数据库监控
+			if(url.contains("/druid/") && 
+					(url.endsWith(".html") || url.endsWith("druid/submitLogin") || url.endsWith(".json"))){
+				filterChain.doFilter(request, response);
+	        	return;
+			}
 			//会计公司注册.....zpm增加
 			if(url.endsWith("/wbx/invoice!inputExpBill.action") || url.endsWith("/404.html") || url.endsWith("/500.html") || url.endsWith("/au/image.jsp") || url.endsWith("/do/dz_registered.jsp") || url.endsWith("/do/forgot_password.jsp") || url.endsWith("/do/results.jsp")
 					|| url.endsWith("register_act!sendMessage.action") || url.endsWith("register_act!saveRegistered.action") ){
