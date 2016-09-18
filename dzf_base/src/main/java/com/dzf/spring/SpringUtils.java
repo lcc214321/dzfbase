f78f7978ef3912e79e285ddc79f96891d71a8ba1
zpm <zpm@ynt.com>
2016-09-18 09:27:04 +0800

    zpm修改提交
    Signed-off-by: zpm <zpm@ynt.com>

--- dzf_base/src/main/java/com/dzf/spring/SpringUtils.java
+++ dzf_base/src/main/java/com/dzf/spring/SpringUtils.java
@@ -1,32 +1,36 @@
 package com.dzf.spring;
 
+import com.dzf.pub.cache.ServletRequestCache;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.apache.struts2.ServletActionContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import com.dzf.pub.cache.ServletRequestCache;
-
 public class SpringUtils {
+	public static Object getBean(String beanname) {
+		Object obj = null;
+		try {
+			ApplicationContext ac = WebApplicationContextUtils
+					.getWebApplicationContext(ServletActionContext.getServletContext());
 
-	public SpringUtils() {
-		// TODO Auto-generated constructor stub
+			obj = ac.getBean(beanname);
+		} catch (Exception e) {
+
+		}
+
+		if (obj == null) {
+			obj = SpringContextHolder.getBean(beanname);
+		}
+		return obj;
 	}
-	public static Object getBean(String beanname){
-		ApplicationContext ac = WebApplicationContextUtils
-				.getWebApplicationContext(ServletActionContext
-						.getServletContext());
-		return ac.getBean(beanname);
+
+	public static Object getBean2(String beanName) {
+		HttpServletRequest request = (HttpServletRequest) ServletRequestCache.getInstance().getThreadLocal().get();
+		HttpSession session = request.getSession();
+		ServletContext servletContext = session.getServletContext();
+		ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+		return ctx.getBean(beanName);
 	}
-	
-	 public static Object getBean2(String beanName) {
-	    	HttpServletRequest request = (HttpServletRequest)ServletRequestCache.getInstance().getThreadLocal().get();//ZFRequestFilter.getTlCurrentRequest().get();
-			HttpSession session = request.getSession();
-			ServletContext servletContext = session.getServletContext();		
-			ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(servletContext);
-			return ctx.getBean(beanName);
-	    }
 }
