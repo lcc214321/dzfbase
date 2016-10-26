3dbcf94aaf060fb0bf5783da4343c43a0d3db352
Administrator <Administrator@YL-201608271530>
2016-10-26 16:28:35 +0800

    ztc 增加益世禁止跳转

--- dzf_base/src/main/java/com/dzf/pub/DZFRequestFilter.java
+++ dzf_base/src/main/java/com/dzf/pub/DZFRequestFilter.java
@@ -58,7 +58,7 @@ public class DZFRequestFilter implements Filter {
 	}
 	private boolean isForbiddenRedirect(String url)
 	{
-		return url.endsWith("sys/xjr_sync!sync.action") || url.endsWith("xwwy_app/busidata!dealData.action") || url.contains("wbx/invoice!inputExpBill.action") || url.contains("hessian/voucherService");
+		return url.endsWith("sys/xjr_sync!sync.action") || url.endsWith("xwwy_app/busidata!dealData.action") || url.contains("wbx/invoice!inputExpBill.action") || url.contains("hessian/voucherService") || url.contains("/services/YscsService");
 	}
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws 
 	IOException, ServletException {
