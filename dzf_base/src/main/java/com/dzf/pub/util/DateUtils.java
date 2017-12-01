b5a33ee9db3a2a11c86f4b192e585c59a76d3203
zpm <zpm@ynt.com>
2017-12-01 17:21:25 +0800

    zpm修改提交wzn改
    Signed-off-by: zpm <zpm@ynt.com>

--- dzf_base/src/main/java/com/dzf/pub/util/DateUtils.java
+++ dzf_base/src/main/java/com/dzf/pub/util/DateUtils.java
@@ -4,15 +4,56 @@
  */
 package com.dzf.pub.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.dzf.pub.lang.DZFDate;
 
 
 public class DateUtils {
 	private static Calendar CALENDAR = Calendar.getInstance();
+	
+	/** 存放不同的日期模板格式的sdf的Map */
+	private static ThreadLocal<Map<String, SimpleDateFormat>> sdfMap = new ThreadLocal<Map<String, SimpleDateFormat>>() {
+		protected Map<String, SimpleDateFormat> initialValue() {
+			return new HashMap<String, SimpleDateFormat>();
+		}
+	};
+
+	/**
+	 * 返回一个SimpleDateFormat,每个线程只会new一次pattern对应的sdf
+	 * 
+	 * @param pattern
+	 * @return
+	 */
+	private static SimpleDateFormat getSdf(final String pattern) {
+		Map<String, SimpleDateFormat> tl = sdfMap.get();
+		SimpleDateFormat sdf = tl.get(pattern);
+		if (sdf == null) {
+			sdf = new SimpleDateFormat(pattern);
+			tl.put(pattern, sdf);
+		}
+		return sdf;
+	}
+
+	/**
+	 * 这样每个线程只会有一个SimpleDateFormat
+	 * 
+	 * @param date
+	 * @param pattern
+	 * @return
+	 */
+	public static String format(Date date, String pattern) {
+		return getSdf(pattern).format(date);
+	}
+
+	public static Date parse(String dateStr, String pattern) throws ParseException {
+		return getSdf(pattern).parse(dateStr);
+	}
 
 	public static Date endOfDay(Date date) {
 		Calendar calendar = CALENDAR;
